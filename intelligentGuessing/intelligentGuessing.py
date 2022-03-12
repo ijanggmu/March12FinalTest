1c1dc97b4fe5534b033eaabc0fd969c64f5ee648
ijanggmu <sthaejan00@gmail.com>
2022-03-12 17:55:37 +0545

    v5

--- intelligentGuessing/intelligentGuessing.py
+++ intelligentGuessing/intelligentGuessing.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import re
+data=[]
 df=pd.read_csv(r'C:\Users\GGMU\Desktop\Data Engineer\TEST\March12FinalTest\intelligentGuessing\intelligentGuessingDataSet',encoding='latin-1')
 df=df.set_index('rownum')
 firstname=df.columns.get_loc('firstname')
 d_firstname=df.iloc[:,firstname:firstname+1]
-d_firstname
+d_firstname= d_firstname.values.tolist()
 lastname=df.columns.get_loc('lastname')
 d_lastname=df.iloc[:,lastname:lastname+1]
-d_lastname
+d_lastname= d_lastname.values.tolist()
 email=df.columns.get_loc('email')
 d_email=df.iloc[:,email:email+1]
-d_email
+d_email=d_email.values.tolist()
 pattern=df.columns.get_loc('Email Pattern')
 d_Pattern=df.iloc[:,pattern:pattern+1]
-d_Pattern
+d_Pattern=d_Pattern.values.tolist()
 h = re.findall('[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*', str(df))
 email_users = [ x.split('@')[0] for x in h ]
 email_name=[x.split('.')[0] for x in email_users]
@@ -26,5 +27,9 @@ elif d_lastname:
         pattern = ''.join([pattern, '<11>','<20>'])
     elif d_firstname == email_name[0] and d_lastname.split(' ')[1] == email_name[1]:
         pattern = ''.join([pattern, '<11>','<21>'])
-else d_firstname == email_name[0] and d_lastname == '' and email_name[1] in lastname:
+elif d_firstname == email_name[0] and d_lastname == '' and email_name[1] in lastname:
     pattern = ''.join([pattern, '<11>','<20><21>'])
+df = pd.DataFrame([pattern])
+data.append(df)
+df = pd.concat(data)
+df.to_csv("problemset1_submission.csv", index=False)
